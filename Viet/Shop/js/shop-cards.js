2bfb353033e6bd1e4b569ca2639c8e45742903a5
vanviet183 <vanvietgg183@gmail.com>
2021-12-14 22:22:19 +0700

    add packages + shop

---
+++ Viet/Shop/js/shop-cards.js
@@ -0,0 +1,86 @@
+const shopCards = [
+  {
+    img: "img/shop-card/card1.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$2,000.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card2.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$800.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card3.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$2,000.00",
+    sale: "",
+    btnText: "select options",
+  },
+  {
+    img: "img/shop-card/card4.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$999.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card5.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$2,000.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card6.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$500.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card7.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,000.00",
+    currentPrice: "$2,000.00",
+    sale: "",
+    btnText: "select options",
+  },
+
+  {
+    img: "img/shop-card/card8.jpg",
+    name: "Bora Bora",
+    rate: "\2605\2605\2605\2605\2605",
+    priceOld: "$1,600.00",
+    currentPrice: "",
+    sale: "",
+    btnText: "select options",
+  },
+
+];
+
+const CARDS_SHOP_STORAGE_KEY = "SHOP_CARDS"
+
+localStorage.setItem(CARDS_SHOP_STORAGE_KEY, JSON.stringify(shopCards));
+
